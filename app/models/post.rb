f4c7240a54421974cb4465c2b3efbd749f6ad6a3
Arrey Tabe <arrey2391@gmail.com>
2021-02-20 12:53:30 +0100

    creates post model

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true, length: {maximum: 200}
+
+
+  belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, presence: true
     validates :password, presence: true
+
+    has_many :posts
 end
