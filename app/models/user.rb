2d20e40c87f6de6c11fb636d32a3d23969658f2f
Arrey Tabe <arrey2391@gmail.com>
2021-02-20 12:27:07 +0100

    implements user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, presence: true
+    validates :password, presence: true
 end
